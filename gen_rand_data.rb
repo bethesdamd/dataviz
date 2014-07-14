60a780ee84576c514ad826a8b89ce59548fce7be
vagrant <vagrant@precise64.(none)>
2014-07-14 15:58:09 +0000

    added comments

--- gen_rand_data.rb
+++ gen_rand_data.rb
@@ -1,5 +1,8 @@
 #!/home/vagrant/.rvm/rubies/ruby-2.1.1/bin/ruby
 
+# Utility to generate random data sets for testing. Use in conjuction with '>>' on the command line
+# to create a data file.
+
 a = []
 print "{ \"arr\": ["
 (1..20).each do |x|
